3d7b7e6528325ddfc3b2ac01720bfb23cf0c87a9
xxHRUSHIKESHxx <khrushikesh2002@gmail.com>
2024-04-08 13:36:50 +0530

    added mussa

--- src/app/user/dashboard/loading.js
+++ src/app/user/dashboard/loading.js
@@ -1,35 +1,37 @@
-import "../../../../components/ui/hamster.css"
+import "../../../../components/ui/hamster.css";
 export default function Loading() {
   return (
-
-    <div  style={{
-      marginTop:"20rem",
-      marginLeft:"50rem"
-
-    }}>
-
-<div  aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster  ">
-    <div class="wheel"></div>
-    <div class="hamster">
-      <div class="hamster__body">
-        <div class="hamster__head">
-          <div class="hamster__ear"></div>
-          <div class="hamster__eye"></div>
-          <div class="hamster__nose"></div>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        aria-label="Orange and tan hamster running in a metal wheel"
+        role="img"
+        class="wheel-and-hamster  "
+      >
+        <div class="wheel"></div>
+        <div class="hamster">
+          <div class="hamster__body">
+            <div class="hamster__head">
+              <div class="hamster__ear"></div>
+              <div class="hamster__eye"></div>
+              <div class="hamster__nose"></div>
+            </div>
+            <div class="hamster__limb hamster__limb--fr"></div>
+            <div class="hamster__limb hamster__limb--fl"></div>
+            <div class="hamster__limb hamster__limb--br"></div>
+            <div class="hamster__limb hamster__limb--bl"></div>
+            <div class="hamster__tail"></div>
+          </div>
         </div>
-        <div class="hamster__limb hamster__limb--fr"></div>
-        <div class="hamster__limb hamster__limb--fl"></div>
-        <div class="hamster__limb hamster__limb--br"></div>
-        <div class="hamster__limb hamster__limb--bl"></div>
-        <div class="hamster__tail"></div>
+        <div class="spoke"></div>
       </div>
     </div>
-    <div class="spoke"></div>
-  </div>
-      
-
-    </div>
-    
-  
   );
 }
